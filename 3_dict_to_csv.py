267ce88b95a2062fee67f7aa4aa3e9b28ef44fef
filatov <allaleks@gmail.com>
2021-05-24 16:27:23 +0300

    not done

--- 3_dict_to_csv.py
+++ 3_dict_to_csv.py
@@ -15,7 +15,18 @@ def main():
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    name_dict ={"name":"Ivan","name":"Sergey"}
+    age_dict = {"age":"18", "age":"21","age":"30","age":"40" }
+    job_dict = {"job":"bank","job":"FMCG","job":"Gov","job":"less"}
+    position_dict = {"position":"manager","position":"director","position":"out staff"}
+
+    CV_list = []
+    CV_list.append(name_dict)
+    CV_list.append(age_dict)
+    CV_list.append(job_dict)
+    CV_list.append(position_dict)
+
+    print(CV_list)
 
 if __name__ == "__main__":
     main()
